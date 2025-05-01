bbc402122358abd317df0a49a0f5c20b671beb58
Kerstin Humm <kerstin@erictapen.name>
2025-05-01 12:17:00 +0200

    strichliste-web-frontend: use latest master

--- pkgs/strichliste-web-frontend/package.nix
+++ pkgs/strichliste-web-frontend/package.nix
@@ -10,18 +10,19 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "strichliste-web-frontend";
-  version = "1.7.1";
+  version = "1.7.1-unstable";
 
   src = fetchFromGitHub {
     owner = "strichliste";
     repo = "strichliste-web-frontend";
-    tag = "v${finalAttrs.version}";
-    hash = "sha256-r9R//4XE85dkChLSu+Sn8Yo72dNZY8Z3yDHOiYIYjwg=";
+    # recent master
+    rev = "05c3611d614bf9a41f3c1ac8cf0d53a94bf6712b";
+    hash = "sha256-KpnKz0Iv7Z57SUv4eai/PVOyGK7Lw1WkuzjUjybyCCk=";
   };
 
   offlineCache = fetchYarnDeps {
     yarnLock = "${finalAttrs.src}/yarn.lock";
-    hash = "sha256-NVQpXMiKVgFnAxLvl+BhFqXZU51D2CWfrVs5e/m4bMs=";
+    hash = "sha256-f9Vv+aJ5gFMuLxwOPhFFKjdnE7i/i0Sp0BjFka7nDtE=";
   };
 
   nativeBuildInputs = [
